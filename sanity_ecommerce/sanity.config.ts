1f4a840bee874ee2c0673f9b3be0d81ba125b208
Mohmed S Shaikh <sahilshaikh1595@gmail.com>
2023-03-09 12:21:53 -0500

    Initial Commit

---
+++ sanity_ecommerce/sanity.config.ts
@@ -0,0 +1,22 @@
+import {defineConfig, isDev} from 'sanity'
+import {visionTool} from '@sanity/vision'
+import {deskTool} from 'sanity/desk'
+import {schemaTypes} from './schemas'
+import {getStartedPlugin} from './plugins/sanity-plugin-tutorial'
+
+const devOnlyPlugins = [getStartedPlugin()]
+
+export default defineConfig({
+  name: 'default',
+  title: 'white-gazelle',
+
+  projectId: 't2l0xh12',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool(), ...(isDev ? devOnlyPlugins : [])],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
+
---
+++ sanity_ecommerce/schemas/index.ts
@@ -0,0 +1,5 @@
+import product from './products'
+import banner from './banner'
+
+export const schemaTypes = [product, banner]
+
