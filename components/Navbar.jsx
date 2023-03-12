fc7361ad214589c253d08752538a922370bdf799
Mohmed S Shaikh <sahilshaikh1595@gmail.com>
2023-03-12 00:36:07 -0500

    Main frontend completed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className="navbar-container">
+            <p className="logo">
+                <Link href="/">Mobile Suit Market</Link>
+            </p>
+            <button type="button" classname="cart-icon" onClick="">
+                <AiOutlineShopping />
+                <span className="cart-item-qty">1</span>
+            </button>
+        </div>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,11 @@ const Home = ({ products, bannerData }) => {
         <p>Mobile Suits of many varieties</p>
       </div>
       <div className="products-container">
-        {products?.map((product) => product.name)}
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </div>
   );
 };
